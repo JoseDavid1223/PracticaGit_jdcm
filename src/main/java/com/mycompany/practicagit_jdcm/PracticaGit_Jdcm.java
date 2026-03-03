249153819fa27f3fe6660496a2ac5e05cb7dbe83
marielestrella <mariel.estrella_pm24301@cecyte.edu.mx>
2026-03-03 07:36:42 -0600

    feat: estructura inícial del proyecto.

---
+++ src/main/java/com/mycompany/practicagit_jdcm/PracticaGit_Jdcm.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_jdcm;
+
+/**
+ *
+ * @author Pomuch11
+ */
+public class PracticaGit_Jdcm {
+
+    public static void main(String[] args) {
+        System.out.println ("---Sistema de Gestion");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
