d425194577e870ba3c0c7d1dea0577f94fad27c5
marielestrella <mariel.estrella_pm24301@cecyte.edu.mx>
2026-03-09 07:20:29 -0600

    fix: añadida calculadora basica

--- src/main/java/com/mycompany/practicagit_jdcm/PracticaGit_Jdcm.java
+++ src/main/java/com/mycompany/practicagit_jdcm/PracticaGit_Jdcm.java
@@ -11,7 +11,13 @@ package com.mycompany.practicagit_jdcm;
 public class PracticaGit_Jdcm {
 
     public static void main(String[] args) {
-        System.out.println ("---Sistema de Gestion");
+        System.out.println ("---Sistema de Gestión de Alumnos---");
+        System.out.println ("Bienvenido, Usuario Estudiante.");
+        
+       
+        
+        
+        
         
         
         
